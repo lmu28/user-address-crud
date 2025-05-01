908d190f3ccf9c8a70549a1d5824cfe865e26907
Max <maximus6299@mail.ru>
2025-05-01 21:09:35 +0300

    Init commit

---
+++ src/main/java/com/example/user_address_crud/UserAddressCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_address_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAddressCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAddressCrudApplication.class, args);
+	}
+
+}
